6b2746b8384f05bcf624bfb9ab17200b033404b5
lizutanianca <utani2000@gmail.com>
2016-07-19 17:11:12 +0200

    corregido
    Signed-off-by: quintanalaupaliz <liztafurocampo@gmail.com>

--- mejorado_codigo_simple.ino
+++ mejorado_codigo_simple.ino
@@ -9,15 +9,16 @@ char keys[ROWS][COLS] = {
   {'7','8','9'},
   {'*','0','#'}};
 
-byte rowPins[ROWS] = {7, 6, 5, 4}; //conexion de los pines de las filas del keypad con arduino.
+byte rowPins[ROWS] = {7, 6, 5, 4}; //conexion de los pines de las filas del keypad con arduino uno.
 byte colPins[COLS] = {3, 2, A5}; //conexion de los pines de las columnas del keypad con arduino.
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-//char PIN[6]={'1','2','3','4','5''6'}; //  contraseña (!) 
+//char PIN[6]={'1','2','3','4','5','6'}; //  contraseña (!) 
 char PIN[6]={0,0,0,0,0,0};//pin sin ingresar contraseña
 char contra[6]={0,0,0,0,0,0}; // arreglo para comparar
 int estadoPIN=0;
 int z=0;
 int z1=0;
+
 
