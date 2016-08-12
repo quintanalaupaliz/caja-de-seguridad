1630ed0cb5763892e474a770c5b4e66937026edf
liz utani anca <utani2000@gmail.com>
2016-08-11 21:59:23 -0500

    hay error en la conpilacion falta corregir un poco mas

--- mejorado_codigo_simple/bienhecho.ino
+++ mejorado_codigo_simple/bienhecho.ino
@@ -4,40 +4,32 @@ byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED//direccion mac
 };
 IPAddress ip(192, 168, 1, 177);//la direccion ip sera dependiente
-EthernetServer server(80);//el puerto 80 es automatico
-String header;
-int pin_estado=6;//es el pin de led
-int led_abier=2;//pin de led puerta abierta
-int led_cerrar=3;//pin de led que esta la puerta cerrada
-int led_error=4;// pin de error
-String estado="OFF"; // estado de led inicialmente Off
+
+ubidots client(token);
+int servo_uno = 2;//es el pin para el primer servo
+int servo_dos = 3;//es el pin para el segundo servo
+int led_estado = 4;
+int led_abierto = 5;
+int led_cerrado = 6;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);//iniciar la coneccion de Ethernt
+  if(Ethernet.begin(mac)==0){
+  Serial.print("failed to configure Ethernet using DHCP");
+  //tratar de configurar utilizando la direccion IP en lugar de DHCP
   Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at");
-  Serial.println(Ethernet.localIP());
-  pinMode(pin_estado,OUTPUT);
-  pinMode(led_abier,OUTPUT);
-  pinMode(led_cerrar,OUTPUT);
-  pinMode(led_error,OUTPUT);
+  }
+  pinMode(servo_uno,OUTPUT);
+  pinMode(servo_dos,OUTPUT);
+  pinMode(led_estado,OUTPUT);
+  pinMode(led_abierto,OUTPUT);
+  pinMode(led_cerrado,OUTPUT);
 }
 void loop(){
 
   EthernetClient client =server.available();//termina con un espacio en blanco
-  if (client){
-  Serial.println("new client");
-  boolean currentLineIsBlank = true;
-  String cadena="";
-  while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        header += c;
-        Serial.write(c);
-        cadena.concat(c);
-        int posicion=cadena.indexOf("LED=");
-       if(cadena.substring(posicion)=="LED=ON") 
+  if(estado_puerta == 1){
+   
          {
 
             digitalWrite(pin_estado,HIGH);
@@ -45,10 +37,7 @@ void loop(){
             digitalWrite(led_cerrar,LOW);
             digitalWrite(led_error,LOW);
             estado="ON";
-          }
-
-          if(cadena.substring(posicion)=="LED=OFF")//Si a la posición 'posicion' hay "LED=OFF"
-          {
+          }          
             digitalWrite(pin_estado,LOW);
             digitalWrite(led_abier,LOW);
             digitalWrite(led_cerrar,HIGH);
